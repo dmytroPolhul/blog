0b0cb13090e29c9236c4792eea23b48808363499
Dmytro Polhul <polhul.d@gmail.com>
2023-08-13 20:58:08 +0300

    test(user resolver): more tests

--- src/modules/user/user.resolver.spec.ts
+++ src/modules/user/user.resolver.spec.ts
@@ -16,6 +16,9 @@ describe('UserResolver', () => {
           useValue: {
             createUser: jest.fn() as jest.Mock,
             updateUser: jest.fn() as jest.Mock,
+            deleteUser: jest.fn() as jest.Mock,
+            getUser: jest.fn() as jest.Mock,
+            getUsers: jest.fn() as jest.Mock,
           },
         },
       ],
@@ -77,5 +80,65 @@ describe('UserResolver', () => {
     const result = await resolver.updateUser(request);
     expect(result).toEqual(res);
     expect(userService.updateUser).toHaveBeenCalledWith(request);
+  });
+
+  it('should delete a user', async () => {
+    const existUser = {
+      id: '5d5fc259-9727-416c-bf47-03125c5c5e07',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      password: '1234',
+      status: true,
+      role: Role.MODERATOR,
+    };
+
+    const res = {
+      ...existUser,
+      deletedAt: new Date(),
+      status: false,
+    };
+
+    (userService.deleteUser as jest.Mock).mockResolvedValue(res);
+
+    const result = await resolver.removeUser(existUser.id);
+    expect(result).toEqual(res);
+    expect(userService.deleteUser).toHaveBeenCalledWith(existUser.id);
+  });
+
+  it('should return a user by id', async () => {
+    const existUser = {
+      id: '5d5fc259-9727-416c-bf47-03125c5c5e07',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      password: '1234',
+      status: true,
+      role: Role.MODERATOR,
+    };
+
+    (userService.getUser as jest.Mock).mockResolvedValue(existUser);
+
+    const result = await resolver.findOneUser(existUser.id);
+    expect(result).toEqual(existUser);
+    expect(userService.getUser).toHaveBeenCalledWith(existUser.id);
+  });
+
+  it('should return all users', async () => {
+    const existUser = {
+      id: '5d5fc259-9727-416c-bf47-03125c5c5e07',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      password: '1234',
+      status: true,
+      role: Role.MODERATOR,
+    };
+
+    (userService.getUsers as jest.Mock).mockResolvedValue([existUser]);
+
+    const result = await resolver.findAllUsers();
+    expect(result).toEqual([existUser]);
+    expect(userService.getUsers).toHaveBeenCalled();
   });
 });

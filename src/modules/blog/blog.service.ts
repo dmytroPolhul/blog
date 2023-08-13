5621979eeb117bed796c53447da562e1e1cd56f8
Dmytro Polhul <polhul.d@gmail.com>
2023-08-13 19:26:39 +0300

    fix(blogs): posible get author too

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -91,6 +91,7 @@ export class BlogService extends BaseService<Blog> {
     const [results, total] = await this.blogRepository.findAndCount({
       ...whereOptions,
       ...posts,
+      relations: ['author']
     });
     return {
       results,
